b5c99aec9cbcf5af2dd9594d8260dfbcbb66be5e
Wootten <harrisonwootten@hotmail.com>
2021-09-22 11:53:00 +0100

    Can now have more than 4 walls

--- IdeaProjects/HelloWorld/src/HelloWorld/Main.java
+++ IdeaProjects/HelloWorld/src/HelloWorld/Main.java
@@ -1,6 +1,6 @@
 package HelloWorld;
 import java.util.ArrayList;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,8 +8,6 @@ public class Main {
         Scanner myObj = new Scanner(System.in);
 
         double ceilingLength; double ceilingWidth;
-        double wallLengthA; double wallLengthB; double wallLengthC; double wallLengthD;
-        double wallHeightA; double wallHeightB; double wallHeightC; double wallHeightD;
         double coatsNum; double windowArea;
 
         int paint = 130;
@@ -40,8 +38,6 @@ public class Main {
 
         //System.out.println(windowArea);
 
-        System.out.println("Enter Number of Coats of paint desired");
-        coatsNum = myObj.nextInt();
         double averageDoorSize = 2.5 * 6.5; //door size in sq feet
 
         System.out.println("Enter Number of doors in room");
@@ -54,57 +50,53 @@ public class Main {
         ceilingWidth = myObj.nextDouble();
         double ceilingArea = ceilingLength*ceilingWidth;
 
-        System.out.println("Enter Wall A Length(ft)");
-        wallLengthA = myObj.nextDouble();
-        System.out.println("Enter Wall B Length(ft)");
-        wallLengthB = myObj.nextDouble();
-        System.out.println("Enter Wall C Length(ft)");
-        wallLengthC = myObj.nextDouble();
-        System.out.println("Enter Wall D Length(ft)");
-        wallLengthD = myObj.nextDouble();
+        System.out.println("How many walls does the room have?");
+        int walls = myObj.nextInt();
 
-        System.out.println("Enter Wall A Height(ft)");
-        wallHeightA = myObj.nextDouble();
-        System.out.println("Enter Wall B Height(ft)");
-        wallHeightB = myObj.nextDouble();
-        System.out.println("Enter Wall C Height(ft)");
-        wallHeightC = myObj.nextDouble();
-        System.out.println("Enter Wall D Height(ft)");
-        wallHeightD = myObj.nextDouble();
+        //Asks for total length of each wall, adds them to arrayList
+        ArrayList<Double> lengthOfWalls = new ArrayList<>();
+        for (int i = 1; i <= walls; i++) {
+            System.out.println("Enter Length of Wall " + i);
+            double wallLength = myObj.nextDouble();
+            lengthOfWalls.add(wallLength);
+        }
+        //System.out.println(lengthOfWalls);
 
-        double totalWallLength = wallLengthA + wallLengthB + wallLengthC + wallLengthD;
-        double totalWallHeight = wallHeightA + wallHeightB + wallHeightC + wallHeightD;
+        //Adds together all wall lengths from arrayList
+        double totalWallLength = 0.0;
+        for (int i = 0; i < lengthOfWalls.size(); i++) {
+            totalWallLength = totalWallLength + lengthOfWalls.get(i);
+        }
+        //System.out.println(totalWallLength);
+
+        ArrayList<Double> heightOfWalls = new ArrayList<>();
+        for (int i = 1; i <= walls; i++) {
+            System.out.println("Enter Height of Wall " + i);
+            double wallHeight = myObj.nextDouble();
+            heightOfWalls.add(wallHeight);
+        }
+
+        double totalWallHeight = 0.0;
+        for (int i = 0; i < lengthOfWalls.size(); i++) {
+            totalWallHeight = totalWallHeight + heightOfWalls.get(i);
+        }
+
+        //System.out.println(totalWallHeight);
 
         double wallArea = (totalWallLength*totalWallHeight) - (doorsArea + windowArea);
         System.out.println("Total wall area = " + wallArea + " square feet");
-        System.out.println("Total ceiling area = " + ceilingArea + " square feet");
+        System.out.println("Total ceiling area = " + ceilingArea + " square feet \n");
+
+        System.out.println("Enter Number of Coats of paint desired");
+        coatsNum = myObj.nextInt();
 
         double wallPaint = Math.ceil(wallArea / paint) * coatsNum;
         double ceilingPaint = Math.ceil(ceilingArea / paint) * coatsNum;
         double totalPaint = Math.ceil(wallPaint + ceilingPaint);
+
         System.out.println("Paint required for the walls(excluding woodwork etc) = " + wallPaint + " Litres");
         System.out.println("Paint required for the ceiling = " + ceilingPaint + " Litres");
-        System.out.println("Total Paint for walls and ceiling = " + totalPaint + " Litres");
-
-        System.out.println("How many walls does the room have?");
-        int walls = myObj.nextInt();
-
-        Double[] lengthOfWalls = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-        for (int i = 0; i <= walls; i++) {
-            System.out.println("Enter Length of Wall " + i);
-            double wallLength = myObj.nextDouble();
-            lengthOfWalls[i] = wallLength;
-        }
-        System.out.println(Arrays.toString(lengthOfWalls));
-        /*ArrayList<Double> lengthOfWalls = new ArrayList<>();
-        for (int i = 0; i <= walls; i++) {
-            System.out.println("Enter Length of Wall " + i);
-            double wallLength = myObj.nextDouble();
-            lengthOfWalls.add(wallLength);
-        }*/
-
-        //TO DO: add all items in 'lengthOfWalls' together and append to variable to hold total wall length.
-        //Copy this ^ for Height of the walls, do area calculations with both to conclude program.
+        System.out.println("Total Paint for walls and ceiling = " + totalPaint + " Litres \n");
 
         int time = 30;
         while (time >= 0) {
